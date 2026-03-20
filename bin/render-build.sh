d04a27175485619b309f9cf482992984260803b2
Hugues <hugues@mbp2015.lan>
2026-03-19 23:58:49 -0400

    fix: Node 20 LTS + SECRET_KEY_BASE for Render build
    - Update .node-version from 18.17.1 to 20.18.0
    - Generate SECRET_KEY_BASE during build if not set

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 bundle install
 npm install --legacy-peer-deps
 
+# SECRET_KEY_BASE needed during asset precompilation
+export SECRET_KEY_BASE=${SECRET_KEY_BASE:-$(ruby -rsecurerandom -e 'puts SecureRandom.hex(64)')}
+
 # Build assets
 bundle exec rake assets:precompile
 
